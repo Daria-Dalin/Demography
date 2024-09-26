443d6b37a6f0def982a4bc171cd77789b4afb60a
Dalin <dalina.sytova@gmail.com>
2024-09-26 22:56:58 +0300

    print principles

---
+++ prg3.py
@@ -0,0 +1,15 @@
+#динамическая типизация
+a = 3
+#....
+a = 65.6
+#....
+a = 'Петр'
+
+print (a)
+
+greeting = 'Привет,'
+name = 'Петр'
+surname = 'Васильев'
+age = 28
+print (greeting, name, surname)
+print('Вам', age, 'лет.')

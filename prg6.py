7a79e6dbaeffbceeece8bfe1e4940fc26036894f
Dalin <dalina.sytova@gmail.com>
2024-09-28 11:30:16 +0300

    operators

---
+++ prg6.py
@@ -0,0 +1,28 @@
+donut = input('Сколько стоил пончик: ')
+tea = input('Сколько стоил чай: ')
+total = int(donut) + int(tea)
+print('C вас', total, 'руб.')
+
+a = 3.999
+print(int(a))
+
+a = 4
+b = 2
+res = a / b
+print(int(res))
+
+a = '400'
+res = int(a)
+print = (res + 5)
+
+a = 100
+res = 'Мне ' + str(a) + ' лет!'
+print(res)
+
+string = """
+Привет
+    участникам
+    соревнования
+    
+    """
+print(string)
---
+++ prg7.py
@@ -0,0 +1,11 @@
+#Управляющие последовательности в строках
+#escape sequence
+# / slash
+# \ backslash - начало упр послед
+# \t - табуляция
+string = 'Привет\nучастникам\n\tсоревнования'
+path = 'C:\\Windows\\com.exe'
+conflict = "КАфе \"Аист\""
+print(string)
+print(path)
+print(conflict)

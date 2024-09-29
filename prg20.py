03d5570670eafaef27c52bd6fdd1be0f2b3a4b05
Dalin <dalina.sytova@gmail.com>
2024-09-29 09:33:25 +0300

    length of iterable object

---
+++ prg20.py
@@ -0,0 +1,31 @@
+# pass - Заглушка
+for i in range(5):
+    pass  # TODO
+    print('Привет', i)
+
+for _ in range(5):
+    print('Привет')
+
+string = 'Телевизор'
+# определение числа элементо итерируемого объекта
+
+
+length = len(string)
+# число 5600 - 4-значное
+print('в слове', string, length, 'букв.')
+print('число')
+number = input('Введите целое число: ')
+length = len(number)
+print('Число', number, str(length) + '- x', 'значное.')
+
+total = 0
+
+for item in number:
+    total += int(item)
+
+print('Сумма разрядов числа', number, 'равна:', total)
+
+total = 1
+for item in number:
+    total *= int(item)
+print('Произведение разрядов числа', number, 'равно:', total)

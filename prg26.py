e0ccfb739d85cdc6f847778df263da4bebb02a2c
Dalin <dalina.sytova@gmail.com>
2024-10-02 22:55:38 +0300

    string index

--- prg26.py
+++ prg26.py
@@ -8,6 +8,8 @@ digits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 remove_incorrect = letters - forbidden
 # объединяем с цифрами
 temp = remove_incorrect | digits
+
 temp = list(temp)
+
 for i in range(5):
     print(temp[i], end='') #[] - обратиться по индексу
---
+++ prg27.py
@@ -0,0 +1,11 @@
+#index 012345
+s = 'Python'
+print(s[-1])
+print(s[len(s) - 1])
+
+length = len(s) # для индекса отнимаем 1
+print('Длина строки', s, length)
+
+for ch in s:
+    print(ch, end='')
+

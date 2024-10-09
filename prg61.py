b91da53d19c7a0067d722729c44ab115de02ed47
Dalin <dalina.sytova@gmail.com>
2024-10-09 21:45:17 +0300

    modules intro

--- prg61.py
+++ prg61.py
@@ -1,20 +1,28 @@
 # модули
 import functions as f
 
+
 # from functions import * - все подтянуть
 # from function import summ, sub
 
 def main():
     # если это главный исполняемый модуль
     # global
-    first = 3
-    second = 4
+    try:
+        first = 3
+        second = 4
 
-    print(f.summ(first, second))
-    print(f.sub(first, second))
-    print(f.divide(first, second))
-    print(f.multiply(first, second))
-    print(__name__)
+
+
+
+
+        print(f.summ(first, second))
+        print(f.sub(first, second))
+        print(f.divide(first, second))
+        print(f.multiply(first, second))
+        print(__name__)
+    except:
+        pass
 
 
 if __name__ == '__main__':

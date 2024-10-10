a4eb2d96dd68444c15dfbf7c8e44e57af6d1de7a
Dalin <dalina.sytova@gmail.com>
2024-10-10 22:21:54 +0300

    OOP encapsulation

---
+++ 63.py
@@ -0,0 +1,43 @@
+# private __
+#
+
+class Man:
+    def __init__(self, n='noname', a=0):  # конструктор
+        self.name = n
+        self.age = a
+        self.non_legal_names = ['Васек', 'Павлик']
+
+    def info(self):
+        print(f'{self.name}, возрастом {self.age}')
+
+    # ГЕТТЕРЫ (getters)
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    # сеттер (setter)
+    def set_age(self, new_age):
+        if new_age < 0:
+            self.age = -new_age
+        else:
+            self.age = new_age
+
+    def set_name(self, new_name):
+        if new_name not in self.non_legal_names:
+            self.name = new_name
+        print('имя недопустимо. замены не было.\n'
+              'cписок запрещенных имен', end=': ')
+
+    def print_non_legal_names(self):
+        print(*self.non_legal_names, sep=', ')
+
+
+dima = Man('Дмитрий', 17)
+dima.set_name('Васек')
+dima.set_age(-18)
+# print(dima.age)
+dima.info()
+n = dima.get_name()
+print(n)

a202e25da105f459b76c3377c7c698855b9d3d1f
Dalin <dalina.sytova@gmail.com>
2024-10-13 00:06:02 +0300

    PIL Image Drawing

--- prg68.py
+++ prg68.py
@@ -1,5 +1,6 @@
 # Доп библиотека PIL (pillow)
 from PIL import ImageDraw, Image
+from PIL.ImageFont import ImageFont
 
 # создаем и рисуем
 # 1 холст
@@ -7,7 +8,24 @@ canvas = Image.new('RGB', (200, 200), (255, 255, 255))
 
 # создаем рисовальщик
 draw = ImageDraw.Draw(canvas)
-#рисуем линию
-draw.line((0,0,200,200), fill=(255,0,0), width=3)
-#сохраняем
+# рисуем линию Окна
+draw.line((100, 0, 200, 200), fill='brown', width=3)
+draw.line((0, 100, 200, 100), fill='brown', width=3)
+# рисуем многоугольник
+draw.rectangle((0, 0, 50, 50), outline=(0, 250, 250), width=5)
+# напишем текст
+text = 'окно'
+font = ImageFont.truetype('arial.ttf', size=18)
+draw.text((80, 90), text, font=font)
+# рисуем полигон(треугольник)
+draw.polygon(
+    xy=(
+        (200, 200)
+        (150, 250)
+        (200, 150)
+    ), fill='red', outline=(0, 0, 0)
+)
+# рисуем круг
+draw.ellipse((0,0,200,200), outline='brown', width=5)
+# сохраняем
 canvas.save('line.png', 'PNG')

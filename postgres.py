b98c42ef993d688df62cd394aa69a7f8e099a236
Dalin <dalina.sytova@gmail.com>
2024-11-12 01:36:57 +0300

    offtop postgres

---
+++ postgres.py
@@ -0,0 +1,8 @@
+#pip install psycopg2
+import psycopg2
+connection = psycopg2.connect(user="postgres", password="123",
+                              host="127.0.0.1", port="5432",
+                              database='lib')
+cursor = connection.cursor()
+
+result = cursor.execute("""select id from reader""")

fc93a4797bf2f0954e7a24774f7e4eb75b418aa6
Dalin <dalina.sytova@gmail.com>
2024-11-12 22:36:47 +0300

    postgres

--- postgres.py
+++ postgres.py
@@ -1,8 +1,19 @@
 #pip install psycopg2
 import psycopg2
-connection = psycopg2.connect(user="postgres", password="123",
-                              host="127.0.0.1", port="5432",
-                              database='lib')
-cursor = connection.cursor()
 
-result = cursor.execute("""select id from reader""")
+try:
+    #connection = psycopg2.connect(user="postgres", password="123",
+                                  #host="127.0.0.1", port="5432",
+                                  #database='lib')
+    con = psycopg2.connect(user='postgres', password='123', host='10.10.103.252',
+                           port='5432', database='Lib')
+    print(con)
+
+    cursor = con.cursor()
+    res = cursor.execute("select id from reader;")
+    result_set = cursor.fetchall()
+    for record in result_set:
+        print("Result", record)
+    con.close()
+except:
+    print("error")
